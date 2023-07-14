d1e20c8879b75057fc8be0a87408a1420de1c7f8
Luiz Willner <luizwillner@id.uff.br>
2023-07-13 23:36:24 -0300

    Mais nomes dos atributos de luzes renomeados

--- 11-prof/light.js
+++ 11-prof/light.js
@@ -1,41 +1,55 @@
 export default class Light {
   constructor() {
-    // Luz 1
-    this.pos_1 = vec4.fromValues(2.0, 2.0, 2.0, 1.0);
+    // Luz 1: Amarela
+    this.pos_1 = vec4.fromValues(7.0, -5.0, 3.0, 1.0);
 
-    this.amb_c_1 = vec4.fromValues(1.0, 1.0, 1.0, 1.0);
+    this.amb_c_1 = vec4.fromValues(1.0, 1.0, 0.0, 1.0);
     this.amb_k_1 = 0.2;
 
-    this.dif_c_1 = vec4.fromValues(1.0, 1.0, 1.0, 1.0);
+    this.dif_c_1 = vec4.fromValues(1.0, 1.0, 0.0, 1.0);
     this.dif_k_1 = 0.5;
 
-    this.esp_c_1 = vec4.fromValues(1.0, 1.0, 1.0, 1.0);
+    this.esp_c_1 = vec4.fromValues(1.0, 1.0, 0.0, 1.0);
     this.esp_k_1 = 0.4;
     this.esp_p_1 = 5.0;
+
+    // // Luz 2: Branca
+    // this.pos_2 = vec4.fromValues(7.0, -5.0, 3.0, 1.0);
+
+    // this.amb_c_2 = vec4.fromValues(1.0, 1.0, 0.0, 1.0);
+    // this.amb_k_2 = 0.2;
+
+    // this.dif_c_2 = vec4.fromValues(1.0, 1.0, 0.0, 1.0);
+    // this.dif_k_2 = 0.5;
+
+    // this.esp_c_2 = vec4.fromValues(1.0, 1.0, 0.0, 1.0);
+    // this.esp_k_2 = 0.4;
+    // this.esp_p_2 = 5.0;
+    
 
 
   }
 
   createUniforms(gl, program){
-    const posLoc = gl.getUniformLocation(program, "light_pos");
-    gl.uniform4fv(posLoc, this.pos_1);
+    const posLoc_1 = gl.getUniformLocation(program, "light_pos_1");
+    gl.uniform4fv(posLoc_1, this.pos_1);
 
-    const ambCLoc = gl.getUniformLocation(program, "light_amb_c");
-    gl.uniform4fv(ambCLoc, this.amb_c_1);
-    const ambKLoc = gl.getUniformLocation(program, "light_amb_k")
-    gl.uniform1f(ambKLoc, this.amb_k_1);
+    const ambCLoc_1 = gl.getUniformLocation(program, "light_amb_c_1");
+    gl.uniform4fv(ambCLoc_1, this.amb_c_1);
+    const ambKLoc_1 = gl.getUniformLocation(program, "light_amb_k_1")
+    gl.uniform1f(ambKLoc_1, this.amb_k_1);
 
-    const difCLoc = gl.getUniformLocation(program, "light_dif_c");
-    gl.uniform4fv(difCLoc, this.dif_c_1);
-    const difKLoc = gl.getUniformLocation(program, "light_dif_k")
-    gl.uniform1f(difKLoc, this.dif_k_1);
+    const difCLoc_1 = gl.getUniformLocation(program, "light_dif_c_1");
+    gl.uniform4fv(difCLoc_1, this.dif_c_1);
+    const difKLoc_1 = gl.getUniformLocation(program, "light_dif_k_1")
+    gl.uniform1f(difKLoc_1, this.dif_k_1);
 
-    const espCLoc = gl.getUniformLocation(program, "light_esp_c");
-    gl.uniform4fv(espCLoc, this.pos_1);
-    const espKLoc = gl.getUniformLocation(program, "light_esp_k")
-    gl.uniform1f(espKLoc, this.esp_k_1);
-    const espPLoc = gl.getUniformLocation(program, "light_esp_p")
-    gl.uniform1f(espPLoc, this.esp_p_1);
+    const espCLoc_1 = gl.getUniformLocation(program, "light_esp_c_1");
+    gl.uniform4fv(espCLoc_1, this.pos_1);
+    const espKLoc_1 = gl.getUniformLocation(program, "light_esp_k_1")
+    gl.uniform1f(espKLoc_1, this.esp_k_1);
+    const espPLoc_1 = gl.getUniformLocation(program, "light_esp_p_1")
+    gl.uniform1f(espPLoc_1, this.esp_p_1);
   }
 
   updateLight() {
--- 11-prof/phong.frag.js
+++ 11-prof/phong.frag.js
@@ -2,17 +2,17 @@ export default
 `#version 300 es
 precision highp float;
 
-uniform vec4 light_pos;
+uniform vec4 light_pos_1;
 
-uniform vec4 light_amb_c;
-uniform float light_amb_k;
+uniform vec4 light_amb_c_1;
+uniform float light_amb_k_1;
 
-uniform vec4 light_dif_c;
-uniform float light_dif_k;
+uniform vec4 light_dif_c_1;
+uniform float light_dif_k_1;
 
-uniform vec4 light_esp_c;
-uniform float light_esp_k;
-uniform float light_esp_p;
+uniform vec4 light_esp_c_1;
+uniform float light_esp_k_1;
+uniform float light_esp_p_1;
 
 uniform mat4 u_model;
 uniform mat4 u_view;
@@ -37,7 +37,7 @@ void main()
   viewNormal = normalize(viewNormal);
 
   // posição da luz no sistema da câmera
-  vec4 viewLightPos = u_view * light_pos;
+  vec4 viewLightPos = u_view * light_pos_1;
 
   // direção da luz
   vec4 lightDir = normalize(viewLightPos - viewPosition);
@@ -50,8 +50,8 @@ void main()
 
   // fator da componente especular
   vec4 halfVec = normalize(lightDir + cameraDir);
-  float fatorEsp = pow(max(0.0, dot(halfVec, viewNormal)), light_esp_p);
+  float fatorEsp = pow(max(0.0, dot(halfVec, viewNormal)), light_esp_p_1);
 
   // cor final do vértice
-  minhaColor = 0.25 * fColor + 0.75 * (light_amb_k * light_amb_c + fatorDif * light_dif_k * light_dif_c + fatorEsp * light_esp_k * light_esp_c);
+  minhaColor = 0.25 * fColor + 0.75 * (light_amb_k_1 * light_amb_c_1 + fatorDif * light_dif_k_1 * light_dif_c_1 + fatorEsp * light_esp_k_1 * light_esp_c_1);
 }`
